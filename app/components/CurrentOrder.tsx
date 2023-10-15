3706769bb9703a3dbab80ad3d299b6814ace11fb
Alex <58567680+atbu@users.noreply.github.com>
2023-10-15 18:14:13 +0100

    Changed type of price to 2dp and number

--- app/components/CurrentOrder.tsx
+++ app/components/CurrentOrder.tsx
@@ -5,9 +5,9 @@ const CurrentOrder = () => {
   return (
     <div>
       <ul className='pt-1'>
-        <OrderItem name="Cheeseburger" size="Large" price="£8.00" requirements="No Pickle"></OrderItem>
-        <OrderItem name="Fries" price="£8.00" size="Medium"></OrderItem>
-        <OrderItem name="Coca-Cola" price="£8.00" size="Large"></OrderItem>
+        <OrderItem name="Cheeseburger" size="Large" price={8} requirements="No Pickle"></OrderItem>
+        <OrderItem name="Fries" price={8} size="Medium"></OrderItem>
+        <OrderItem name="Coca-Cola" price={8} size="Large"></OrderItem>
       </ul>
       <div className='absolute bottom-2 right-2 left-2'>
           <button className='btn btn-secondary btn-block h-20'>PAY</button>
--- app/components/OrderItem.tsx
+++ app/components/OrderItem.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const OrderItem = ( { name, size, price, requirements="" }: { name: string; size: string; price: string; requirements?: string }) => {
+const OrderItem = ( { name, size, price, requirements="" }: { name: string; size: string; price: number; requirements?: string }) => {
   return (
     <div>
       <li className='pl-3 pt-1 inline-block'>
           - {name}
-          <p className='badge badge-ghost ml-2'>{price}</p>
+          <p className='badge badge-ghost ml-2'>{'£' + price.toFixed(2).toString()}</p>
           <p className='badge badge-primary ml-2'>{size}</p>
           { requirements != "" ? <p className='badge badge-accent ml-2'>{requirements}</p> : null }
       </li>
